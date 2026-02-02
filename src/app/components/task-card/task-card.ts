f5959ddea43045c16d3a4762d5d3fb5b780fe35d
LuciusHx <luciushebert13@gmail.com>
2026-02-02 16:32:09 -0300

    feat: add three-dot action menu to task card

--- src/app/components/task-card/task-card.ts
+++ src/app/components/task-card/task-card.ts
@@ -1,14 +1,17 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, input, output, signal, HostListener, ElementRef, inject } from '@angular/core';
 import { CdkDrag } from '@angular/cdk/drag-drop';
 import { Task } from '../../models/task.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-card',
-  imports: [CdkDrag],
+  imports: [CdkDrag, FormsModule],
   templateUrl: './task-card.html',
   styleUrl: './task-card.scss',
 })
 export class TaskCard {
+  private elementRef = inject(ElementRef);
+
   task = input.required<Task | null>();
 
   taskDetails = output();
@@ -17,7 +20,32 @@ export class TaskCard {
 
   delete = output<string>();
 
+  menuOpen = signal(false);
+
+  //escuta qualquer clique na página
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    //verifica se o clique foi dentro do component
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+
+    //se foi fora, fecha o menu
+    if (!clickedInside) {
+      this.menuOpen.set(false);
+    }
+  }
+
+  toggleMenu(event: MouseEvent) {
+    event.stopPropagation();
+    this.menuOpen.update((v) => !v);
+  }
+
+  onEdit() {
+    this.editTask.emit();
+    this.menuOpen.set(false);
+  }
+
   onDelete() {
     this.delete.emit(this.task()!.id);
+    this.menuOpen.set(false);
   }
 }
